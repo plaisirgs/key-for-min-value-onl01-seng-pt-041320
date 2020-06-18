8cfc12df8ccf6ec64f22b3cd012c4d01815788f5
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-18 09:33:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,30 +1,16 @@
-# require 'pry'
-def key_for_min_value(hash)
-  smallest_key = nil
-  smallest_value = nil
-  
-  hash.each do |key, value|
-    if smallest_value.nil? || value < smallest_value
-      smallest_key = key
-      smallest_value = value
-    end
-  end
-  smallest_key
-end
 
-
+# def key_for_min_value(hash)
+#   smallest_key = nil
+#   smallest_value = nil
   
-  
-#   smallest = {} 
 #   hash.each do |key, value|
-#     if smallest.any?
-#       smallest["s_key"] = key
-#       smallest["s_value"] = value
-#     else
-#       if value < smallest["s_value"]
-#         smallest["s_value"] = value
-#       end
-#       end
+#     if smallest_value.nil? || value < smallest_value
+#       smallest_key = key
+#       smallest_value = value
+#     end
 #   end
-#   binding.pry
+#   smallest_key
 # end
+
+
+  
